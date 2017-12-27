5fc60e441fae8a66cf4dca0b46e494e14e5c4e17
weimingchuan <1107963848@qq.com>
2017-12-27 20:20:56 +0800

    发布到jcenter

--- app/src/main/java/com/example/love/buildmavendemo/MainActivity.java
+++ app/src/main/java/com/example/love/buildmavendemo/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.love.buildmavendemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.example.testlibrary.GradleTest;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GradleTest gradleTest = new GradleTest();
+//        GradleTest gradleTest = new GradleTest();
 
     }
 }
