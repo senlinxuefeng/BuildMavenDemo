8db5750cffd4d1d7ffc1dc538d41bc91774118af
weimingchuan <1107963848@qq.com>
2017-12-27 14:31:37 +0800

    已经可以打包成本地的arr包

---
+++ testlibrary/src/main/java/com/example/testlibrary/GradleTest.java
@@ -0,0 +1,11 @@
+package com.example.testlibrary;
+
+/**
+ * Created by love on 2017/12/27.
+ */
+
+public class GradleTest {
+    public void display(){
+        System.out.println("gradle test");
+    }
+}
