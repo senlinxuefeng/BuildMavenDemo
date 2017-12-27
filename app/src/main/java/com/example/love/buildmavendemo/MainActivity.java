e94e6d2b55fbe78c8ddb5d559841a74df24ffc1c
weimingchuan <1107963848@qq.com>
2017-12-27 15:05:03 +0800

    NND竟然放错了位置，fuck啊

--- app/src/main/java/com/example/love/buildmavendemo/MainActivity.java
+++ app/src/main/java/com/example/love/buildmavendemo/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.love.buildmavendemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.testlibrary.GradleTest;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GradleTest gradleTest = new GradleTest();
+
     }
 }
